456d1a3ead91676d449b519d6a6ae7126432bf95
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-05-30 15:17:35 +0530

    day 1

--- Problems/TriangleType.java
+++ Problems/TriangleType.java
@@ -2,26 +2,25 @@ package Problems;
 
 import java.util.Scanner;
 public class TriangleType {
+    public static void main(String[] args) {
+        System.out.print("Enter the sides of triangle: ");
+        Scanner my = new Scanner(System.in);
 
-    System.out.print("Enter the sides of triangle: ")
-    Scanner my = new Scanner(System.in);
+        int side1 = my.nextInt();
+        int side2 = my.nextInt();
+        int side3 = my.nextInt();
 
-    int side1 = my.nextInt();
-    int side2 = my.nextInt();
-    int side3 = my.nextInt();
-
-    if (side1 > 0 && side2 > 0 && side3 > 0){
-        if(side1 == side2 == side3){
-            System.out.print("The triangle is a Equilateral Triangle");
+        if (side1 > 0 && side2 > 0 && side3 > 0) {
+            if (side1 == side2 && side2 == side3) {
+                System.out.print("The triangle is Equilateral Triangle");
+            } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+                System.out.print("The triangle is Isoscles");
+            } else {
+                System.out.print("The triangle is Scalene");
+            }
         }
-        else if (side1 == side2 || side2 == side3 || side1 == side3){
-            System.out.print("This is a isoscles triangle");
+        else{
+            System.out.print("The triangle is not possible");
+            }
         }
-        else {
-            System.out.print("The triangle is Scaline");
         }
-    }else{
-        System.out.print("The triangle is invalid")
-    }
-
-}
