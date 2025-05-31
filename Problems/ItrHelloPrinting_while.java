376e5261da5748eb0db0bad0f118dfd1a9a9f871
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-05-31 10:48:15 +0530

    day 2

--- Problems/ItrHelloPrinting_while.java
+++ Problems/ItrHelloPrinting_while.java
@@ -1,5 +1,19 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class ItrHelloPrinting_while {
+    public static void main(String[] args) {
+        System.out.println("Enter no of times Hello should be printed");
+
+        Scanner mu = new Scanner(System.in);
+        int n = mu.nextInt();
+
+        int i=1;
+        while (i<=n){
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
 
 }
