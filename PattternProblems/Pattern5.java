c137971389e32d471c29c9eace4cd87af56066c1
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-06-01 11:13:24 +0530

    day 2

--- PattternProblems/Pattern5.java
+++ PattternProblems/Pattern5.java
@@ -3,6 +3,28 @@ package PattternProblems;
 public class Pattern5 {
     public static void main(String[] args) {
 
+        int n = 7;
+        int trows = n;
 
+        int spaces = n/2;
+        int stars = 1;
+
+        for (int row = 1 ;row<=trows;row++){
+            for (int csp=1;csp<=spaces;csp++){
+                System.out.print(" ");
+            }
+            for (int cst = 1;cst<=stars;cst++){
+                System.out.print("*");
+            }
+            System.out.println();
+            if (row <=trows/2){
+                spaces--;
+                stars +=2;
+
+            }else{
+                spaces++;
+                stars -=2;
+            }
+        }
     }
 }
