a8f3a82d1b884754459c0de8278f73860d5807ce
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-06-03 21:15:20 +0530

    day 3

---
+++ Problems/MatrixMultiplication.java
@@ -0,0 +1,20 @@
+package Problems;
+
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+        int [][] A = {
+                {1, 3, 4},
+                {3, 2, 1},
+                {2, 13, 2},
+        };
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+
+
+    }
+}
