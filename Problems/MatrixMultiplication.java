52a876cbb7d3c82ddad065ca9f983b9a1b5c18f9
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-06-03 21:31:07 +0530

    day 3

--- Problems/MatrixMultiplication.java
+++ Problems/MatrixMultiplication.java
@@ -9,11 +9,33 @@ public class MatrixMultiplication {
                 {3, 2, 1},
                 {2, 13, 2},
         };
-        int[][] arr = {
+        int[][] B = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9},
         };
+
+
+        if (A[0].length != B.length){
+            System.out.println("Matrixx multiplication is not Possible");
+        }
+
+        int [][] result = new int[A.length][B[0].length];
+
+        for (int i = 0; i < A.length;i++){
+            for (int j = 0 ; j < B[0].length;j++){
+                for (int k = 0 ; k < A[0].length;k++){
+                    result[i][j] += A[i][j]*B[k][j];
+                }
+            }
+        }
+        System.out.println("Result of matrix multiplication:");
+        for (int []row : result){
+            for (int val : row){
+                System.out.print(val+ " ");
+            }
+            System.out.println();
+        }
 
 
     }
